6fbd32355140359655ac66f104684ab125cbacd1
Troymendous <troy.wilson.was.taken@gmail.com>
2025-03-11 18:12:30 +1100

    little cleaning

--- vueweather/src/App.vue
+++ vueweather/src/App.vue
@@ -12,5 +12,15 @@ import {router} from "viewrouter";
 
 <style lang="scss" scoped>
 
-</style>
 
+
+#app { 
+  font-family: 'avenir', Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
