7add32c37c6ca935ab6f04f0e461106d0ee46fcd
Troymendous <troy.wilson.was.taken@gmail.com>
2025-03-16 20:28:09 +1100

    patching css

--- vueweather/src/App.vue
+++ vueweather/src/App.vue
@@ -1,26 +1,50 @@
 
 <template>
-  <div>
-<router-view />
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" name="" id="" class="searchbar" placeholder="Search...">
+      </div>
+    </main>
+
 
   </div>
 </template>
 
-<script setup>
-import {router} from "viewrouter";
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      api_key: '077137dcb2faa98dd797e0a1c8b763ce'
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 
-
-
-#app { 
-  font-family: 'avenir', Arial, Helvetica, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+body {
+  font-family: 'monserrat', sans-serif;
+}
+
+#app {
+  background-image: url(.assets/cold-bg.jpg);
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+}
+
 
 </style>
